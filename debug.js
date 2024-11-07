407dc74915c8e75b006a4ae3878454270b970efd
mk-050 <yoshikawa.0505@outlook.jp>
2024-11-07 23:15:43 +0900

    変更

--- debug.js
+++ debug.js
@@ -3,7 +3,7 @@ function toOneDimension(previousValue,currentValue){
     return previousValue.concat(currentValue);
 }
 
-var sampleArrayA[
+var sampleArrayA=[
     ['A1','A2','A3'],
     ['B1','B2','B3'],
     ['C1','C2','C3']
